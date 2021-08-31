a087fcf89f0ecf4a62458fe94f1c53502a833aed
SovanRHong <jaysonsovanhong@gmail.com>
2021-08-31 16:19:00 -0400

    callback methods

--- DOM_Events_CODE/ChangeAndInputEvents/app.js
+++ DOM_Events_CODE/ChangeAndInputEvents/app.js
@@ -1,10 +1,27 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
 // input.addEventListener('change', function (e) {
 //     console.log("CASKDJASKJHD")
 // })
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
+input.addEventListener("input", function (e) {
+  h1.innerText = input.value;
+});
+
+
+
+<body>
+    <h1>Enter Your Username</h1>
+    <input type="text" id="username">
+</body>
+
+</html>
+
+input.addEventListener('input', (e) => {
+    if(!this.value){
+        h1.textContent = `Enter Your Username`
+    } else{
+        h1.textContent = `Welcome, ${input.value}`;
+    }
 })
--- DOM_Events_CODE/FormEvents/app.js
+++ DOM_Events_CODE/FormEvents/app.js
@@ -1,25 +1,60 @@
+const tweetForm = document.querySelector("#tweetForm");
+const tweetsContainer = document.querySelector("#tweets");
+tweetForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-
-const tweetForm = document.querySelector('#tweetForm');
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
+  // const usernameInput = document.querySelectorAll('input')[0];
+  // const tweetInput = document.querySelectorAll('input')[1];
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = tweetForm.elements.tweet;
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = "";
+  tweetInput.value = "";
 });
 
 const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
-}
+  const newTweet = document.createElement("li");
+  const bTag = document.createElement("b");
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(`- ${tweet}`);
+  tweetsContainer.append(newTweet);
+};
 
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector("form");
+const product = document.querySelector("#product");
+const quanity = document.querySelector("#qty");
+
+// form.addEventListner("submit", function (e) {
+//   e.preventDefault();
+
+//   const productInput = product.elements.prdt;
+//   const quanityInput = quanity.elements.qtys;
+
+//   addInputValue(productInput, quanityInput);
+//   productInput.value = "";
+//   quanityInput.value = "";
+// });
+
+// const addInput = (prdt, qtys) => {
+//   const newPrdt = documents.createElements("li");
+//   const newQtys = documents.createElements("li");
+//   newPrdt.append(prdt);
+//   newQtys.append(qtys);
+// };
+
+const form = document.querySelector("form");
+const product = document.querySelector("#product");
+const quantity = document.querySelector("#qty");
+
+const groceryList = document.querySelector("#list");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newList = document.createElement("li");
+  newList.innerHTML = `${quantity.value} ${product.value}`;
+  groceryList.appendChild(newList);
+  quantity.value = "";
+  product.value = "";
+});
