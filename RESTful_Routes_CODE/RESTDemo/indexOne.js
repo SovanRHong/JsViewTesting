ec7a723c1ec0a85a639b2a0e0802d936102de336
SovanRHong <jaysonsovanhong@gmail.com>
2021-09-15 18:04:25 -0400

    sample translate text

---
+++ RESTful_Routes_CODE/RESTDemo/indexOne.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+let comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that is so funny!",
+  },
+  {
+    id: uuid(),
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: uuid(),
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+app.listen(3000, () => {
+  console.log("on port 3000!");
+});
